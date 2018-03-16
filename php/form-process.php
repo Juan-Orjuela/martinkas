10a3886bf93c9d8ef855651999c839184be47983
Juan Orjuela <orjuelajuans@gmail.com>
2018-03-16 16:36:02 -0500

    Cambio de script de formulario

---
+++ php/form-process.php
@@ -0,0 +1,32 @@
+<?php
+$name = $_POST["nombre"];
+$email = $_POST["correo"];
+$message = $_POST["mensaje"];
+ 
+$EmailTo = "contacto@martinkas.com.co";
+$Subject = "Nuevo mensaje desde sitio web";
+ 
+// prepare email body text
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+ 
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+ 
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+ 
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+ 
+// redirect to success page
+if ($success){
+   echo "success";
+}else{
+    echo "invalid";
+}
+ 
+?>
